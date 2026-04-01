3950a009eccc8476e09392787db375227cd90994
Michelle Sanford <mrsanford@wm.edu>
2026-04-01 14:17:16 -0400

    more changes

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 
 _SELF_DIR = Path(__file__).resolve().parent
-ROOT_DIR = _SELF_DIR.parent
+ROOT_DIR = _SELF_DIR.parent.parent.parent
 
 DATA_DIR = ROOT_DIR / "data"
-OUTPUT_DIR = ROOT_DIR / "rasters"
-RAW_DIR = DATA_DIR / "raw"
+# RAW_DIR = DATA_DIR / "raw"
 
 # -- TIFF Specifics ---
+OUTPUT_DIR = ROOT_DIR / "visualization"
 MASK_DIR = OUTPUT_DIR / "mask"
 DISTANCE_DIR = OUTPUT_DIR / "distance"
 
-for folder in [DATA_DIR, OUTPUT_DIR, RAW_DIR, MASK_DIR, DISTANCE_DIR]:
+for folder in [DATA_DIR, OUTPUT_DIR, MASK_DIR, DISTANCE_DIR]:
     folder.mkdir(parents=True, exist_ok=True)
 
 CRS = "EPSG:4326"
