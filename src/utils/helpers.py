66dbdaac5d9a96784b0157b170370d91a3f66e70
Michelle Sanford <mrsanford@wm.edu>
2026-04-08 12:36:24 -0400

    adding constants

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -4,7 +4,6 @@ _SELF_DIR = Path(__file__).resolve().parent
 ROOT_DIR = _SELF_DIR.parent.parent.parent
 
 DATA_DIR = ROOT_DIR / "data"
-# RAW_DIR = DATA_DIR / "raw"
 
 # -- TIFF Specifics ---
 OUTPUT_DIR = ROOT_DIR / "visualization"
@@ -19,3 +18,22 @@ LON_MIN, LON_MAX = -180.0, 180.0
 LAT_MIN, LAT_MAX = -90.0, 90.0
 RES_SUFFIX = "res_0002"
 REGION = "USA_VA"
+
+# Be able to adjust the tile size (level 8 tiles vs level 6 tiles) and not just the pixel size
+# Mess with halo adjuster (at 1 tile right now, allow fractional tiles)
+
+# Test Case Example
+# 80% halo tile overhang on level 4
+
+OSM_HIGHWAYS = [
+    "motorway",
+    "motorway_link",
+    "trunk",
+    "trunk_link",
+    "primary",
+    "primary_link",
+    "secondary",
+    "secondary_link",
+    "tertiary",
+    "tertiary_link",
+]
